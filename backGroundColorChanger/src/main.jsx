be73b8f8f7d2d60c4891ecff81d811758e4c3d4d
VAMSHI KUSUMA <vamshikusuma4846@gmail.com>
2024-11-21 21:17:33 +0530

    add extra apps

--- backGroundColorChanger/src/main.jsx
+++ backGroundColorChanger/src/main.jsx
@@ -6,5 +6,7 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App> this is app </App>
+    <App> this is app </App>
+    <App> this is app </App>
   </StrictMode>,
 )
